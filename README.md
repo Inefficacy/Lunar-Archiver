920dd5300520bec6a90ea964f66dd9fec41a460d
Inefficacy <inefficacyfr@gmail.com>
2022-08-17 13:00:22 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Lunar Client Archiver
+## An easy to use tool to archive versions of [Lunar Client](https://lunarclient.com/)
+## Prerequisites
+
+ - Python 3
+## Getting Started
+ - Install requirements using `python3 -m pip install -r requirements.txt`
+ - Setup configuration in `config.json`
+ - Run `archive.py` with Python 3
+## Configuration
+|Name| Values | Description |
+|--|--|--|
+|  |  |  |
+
+## To-Do
+ - [x] API
+ - [ ] Check SHA1 hash when downloading an artifact
---
+++ lcapi.py
@@ -0,0 +1,35 @@
+from urllib import request
+import requests
+import colorama
+import hashlib
+
+class LcAPI:
+	def print_(self, type, state, message):
+		if self.verbose == True:
+			typed = {0: colorama.Back.GREEN, 1: colorama.Back.YELLOW, 2: colorama.Back.RED, 3: colorama.Back.CYAN}
+			rst = colorama.Style.RESET_ALL
+			print(f'{typed[type]}{state}{rst} {message}')
+	def __init__(self, json, verbose=True):
+		colorama.init(autoreset=True)
+		self.json = json
+		self.verbose = verbose
+	def download(self):
+		self.print_(3, 'download', 'starting')
+		try:
+			r = requests.post('https://api.lunarclientprod.com/launcher/launch', json=self.json)
+		except requests.exceptions.RequestException as e:
+			self.print_(2, 'download', e)
+			return False
+		if r.status_code == 200 and r.json() != None:
+			self.print_(1, 'download', 'finished downloading')
+		else:
+			self.print_(2, 'download', f'status code {r.status_code}')
+			return False
+		self.rjson = r.json()
+		return r.content
+	def downloadArtifact(self, name, checkSHA1=False):
+		self.print_(3, 'download-artifact', name)
+		for artifact in self.rjson['launchTypeData']['artifacts']:
+			if artifact['name'] == name:
+				self.print_(0, 'download-artifact', f'found artifact {name}')
+				r = request.get()
